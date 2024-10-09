fa0bb29043ae837407de6b313b34845977c99e22
Guy haiby <ghaiby@ucsc.edu>
2024-10-09 15:00:14 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement('button');
+button.innerText =  '🏈';
+app.append(button);
+button.addEventListener('click', () => {
+    console.log("click")
+});
