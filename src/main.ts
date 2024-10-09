493b85bc674274a759ec4ff22e7aecbffe315c43
Guy haiby <ghaiby@ucsc.edu>
2024-10-09 15:12:47 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,23 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing new game";
+const gameName = "Football Clicker";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement('button');
-button.innerText =  '🏈';
+let clicks: number = 0; 
+const button = document.createElement("button");
+button.innerText = "🏈";
 app.append(button);
-button.addEventListener('click', () => {
-    console.log("click")
+button.addEventListener("click", () => {
+    clicks++;
+    countDiv.innerText = `Touchdowns: ${clicks}`;
 });
+
+
+const countDiv = document.createElement('div');
+countDiv.innerText = 'Touchdowns: 0';
+app.append(countDiv)
