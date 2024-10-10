137ecee05c0651f25b86d023a9a9e7ccc900aa81
Guy haiby <ghaiby@ucsc.edu>
2024-10-09 23:38:25 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// add clicker button
 let clicks: number = 0;
 const button = document.createElement("button");
 button.innerText = "🏈";
@@ -18,11 +19,30 @@ button.addEventListener("click", () => {
   countDiv.innerText = `Touchdowns: ${clicks}`;
 });
 
+//Add div to show count
 const countDiv = document.createElement("div");
 countDiv.innerText = "Touchdowns: 0";
 app.append(countDiv);
 
+//automatic one second clicker
 setInterval(() => {
-    clicks++;
-    countDiv.innerText = `Touchdowns: ${clicks}`;
+  clicks++;
+  countDiv.innerText = `Touchdowns: ${clicks}`;
 }, 1000);
+
+
+//continuous growth
+let lastTimestamp = performance.now();
+function updateCounter(timestamp: number) {
+    const deltaTime = (timestamp - lastTimestamp) / 1000; 
+    lastTimestamp = timestamp;
+
+    clicks += deltaTime; 
+    countDiv.innerText = `Touchdowns: ${clicks}`;
+
+    requestAnimationFrame(updateCounter);
+}
+
+requestAnimationFrame(updateCounter);
+
+
