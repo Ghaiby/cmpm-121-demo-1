ea8cf2bf932f537efbd8240f44819dd06f21cc7e
Guy haiby <ghaiby@ucsc.edu>
2024-10-14 00:47:00 -0700

    Step 10 Complete

--- src/main.ts
+++ src/main.ts
@@ -33,40 +33,42 @@ app.append(countDiv);
 //upgrade buttons
 
 interface Item {
-    name: string,
-    cost: number,
-    rate: number,
-    index: number
-};
-  
-const availableItems : Item[] = [
-    {name: "RUN", cost: 10, rate: 0.1, index: 0},
-    {name: "PASS", cost: 100, rate: 2, index: 1},
-    {name: "HAIL MARRY", cost: 1000, rate: 50, index: 2},
+  name: string;
+  cost: number;
+  rate: number;
+  index: number;
+  desc: string;
+}
+
+const availableItems: Item[] = [
+  { name: "RUN", cost: 10, rate: 0.1, index: 0 ,desc: "Old school power football" },
+  { name: "PASS", cost: 100, rate: 2, index: 1 ,desc: "WR is down there somewhere" },
+  { name: "HAIL MARRY", cost: 1000, rate: 50, index: 2, desc: "A desperation throw" },
+  { name: "PICK 6", cost: 2500, rate: 100, index: 3, desc: "Back all the way" },
+  { name: "OVERTIME TD", cost: 9999, rate: 999, index: 4, desc: "Game Over" },
 ];
-  
+
 let growthRate: number = 0;
 const inventory: number[] = [];
 const buttons: HTMLButtonElement[] = [];
-availableItems.forEach(item => {
-    const B = document.createElement("button");
-    B.className = "upgradeButton";
-    B.name = item.name;
-    B.innerText = `${item.name} \n+${item.rate} auto TD/s: ${item.cost} TDs`;
-    app.append(B);
-    B.addEventListener("click", () => {
-        if (clicks >= item.cost) {
-          clicks -= item.cost; // Deduct cost of the upgrade
-          growthRate += 0.1;
-          item.cost *= 1.15;
-          updateDisplay();
-          inventory[item.index]++;
-        }
-      });
-    buttons.push(B);
-    inventory.push(0);
+availableItems.forEach((item) => {
+  const B = document.createElement("button");
+  B.className = "upgradeButton";
+  B.name = item.name;
+  B.innerText = `${item.name} \n+${item.rate} auto TD/s: ${item.cost} TDs \n "${item.desc}"`;
+  app.append(B);
+  B.addEventListener("click", () => {
+    if (clicks >= item.cost) {
+      clicks -= item.cost; // Deduct cost of the upgrade
+      growthRate += 0.1;
+      item.cost *= 1.15;
+      updateDisplay();
+      inventory[item.index]++;
+    }
+  });
+  buttons.push(B);
+  inventory.push(0);
 });
-
 
 // Update the display and manage the upgrade button's state
 function updateDisplay() {
@@ -75,10 +77,11 @@ function updateDisplay() {
   for (let i = 0; i < availableItems.length; i++) {
     countDiv.innerText += ` ${availableItems[i].name} : ${inventory[i]}`;
   }
-  for(let i = 0; i< availableItems.length; i++){
-    let item = availableItems[i]
+  for (let i = 0; i < availableItems.length; i++) {
+    const item = availableItems[i];
     buttons[i].disabled = clicks < item.cost;
-    buttons[i].innerText = `${item.name} \n+${item.rate} auto TD/s: ${item.cost} TDs`;
+    buttons[i].innerText =
+      `${item.name} \n+${item.rate} auto TD/s: ${item.cost} TDs \n "${item.desc}"`;
   }
 }
 
